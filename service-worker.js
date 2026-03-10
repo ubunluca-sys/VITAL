9b4036c2aef1fd1dd672f967dee93d4bb07d3494
ubunluca-sys <ubunluca@gmail.com>
2026-03-10 17:55:29 +0100

    Update service worker for caching and fetch handling
    Refactor service worker to handle caching and fetch events.

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,26 @@ return cache.addAll([
 "/VITAL/",
 "/VITAL/index.html",
 "/VITAL/manifest.json"
-])
+])self.addEventListener("install", event => {
+event.waitUntil(
+caches.open("vital-cache").then(cache => {
+return cache.addAll([
+"./",
+"./index.html",
+"./manifest.json"
+]);
+})
+);
+});
+
+self.addEventListener("fetch", event => {
+event.respondWith(
+caches.match(event.request).then(response => {
+return response || fetch(event.request);
+})
+);
+});
+
 })self.addEventListener('install', e => {
 e.waitUntil(
 caches.open('vital-cache').then(cache => {
