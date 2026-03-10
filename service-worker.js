dc6e00f3b51b9e5243d08befdf75ae8c76314502
ubunluca-sys <ubunluca@gmail.com>
2026-03-10 17:40:12 +0100

    Add service worker for caching and fetch handling

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("vital-cache").then(cache => {
+      return cache.addAll([
+        "/VITAL/",
+        "/VITAL/index.html",
+        "/VITAL/manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
